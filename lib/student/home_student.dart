f0edac705f4525b08b9e5a01d497cbd4804dd06f
AbdulRafay08 <rafay51698@gmail.com>
2021-12-02 11:59:36 +0500

    Third Commit

--- lib/student/home_student.dart
+++ lib/student/home_student.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/theme/app_color.dart';
 
 class HomeStudent extends StatefulWidget {
   const HomeStudent({Key? key}) : super(key: key);
@@ -10,6 +11,57 @@ class HomeStudent extends StatefulWidget {
 class _HomeStudentState extends State<HomeStudent> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: white,
+      appBar: AppBar(
+        backgroundColor: primary,
+        foregroundColor: white,
+        title: const Text(
+          'Student Data',
+          style: TextStyle(fontSize: 25),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => const LoginStudent(),
+                //   ),
+                // );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: primary,
+                ),
+                margin: const EdgeInsets.all(15),
+                height: MediaQuery.of(context).size.height * 0.3,
+                width: MediaQuery.of(context).size.width * 0.85,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: 90,
+                      width: 135,
+                      child: Image.asset("assets/student1.png"),
+                    ),
+                    const Text(
+                      "STUDENT",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
